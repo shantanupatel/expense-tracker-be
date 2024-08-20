be2733e2bbc64164f140701c72b8892f179a351c
Shantanu Patel <shantu.patel@gmail.com>
2024-08-20 14:03:28 +0530

    CategoryController created for returning list of categories.

---
+++ expense-tracker-be/src/main/java/com/expense_tracker_be/controller/CategoryController.java
@@ -0,0 +1,21 @@
+package com.expense_tracker_be.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CategoryController {
+
+	@GetMapping(path = "categories")
+	public List<String> getCategories() {
+		ArrayList<String> categories = new ArrayList<>();
+
+		categories.add("Category 1");
+		categories.add("Category 2");
+
+		return categories;
+	}
+}
