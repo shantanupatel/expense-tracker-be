b7585b2d7a94628271ec3c791e0ed90c97548d64
Shantanu Patel <shantu.patel@gmail.com>
2024-08-20 13:03:10 +0530

    Initial project setup done using Spring Initializer.

---
+++ expense-tracker-be/src/main/java/com/fs_social_be/ExpenseTrackerBeApplication.java
@@ -0,0 +1,13 @@
+package com.fs_social_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseTrackerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseTrackerBeApplication.class, args);
+	}
+
+}
---
+++ expense-tracker-be/src/test/java/com/fs_social_be/ExpenseTrackerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fs_social_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
