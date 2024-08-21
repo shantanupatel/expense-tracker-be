08dcea58f11ac151d2eab67f87284eccdbb6da61
Shantanu Patel <shantu.patel@gmail.com>
2024-08-21 11:29:24 +0530

    Category entity updated to make use of entity annotation for table creation.

--- expense-tracker-be/src/main/java/com/expense/model/Category.java
+++ expense-tracker-be/src/main/java/com/expense/model/Category.java
@@ -2,8 +2,16 @@ package com.expense.model;
 
 import java.time.LocalDateTime;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Category {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String categoryName;
 	private String createdBy;
