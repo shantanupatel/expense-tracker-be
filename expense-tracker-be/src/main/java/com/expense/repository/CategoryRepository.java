fa7d4a7e9acf81809057f0cfe68a78567320dafd
Shantanu Patel <shantu.patel@gmail.com>
2024-08-21 11:27:40 +0530

    CategoryRepository class file updated with annotations for Repository and Transactional

---
+++ expense-tracker-be/src/main/java/com/expense/repository/CategoryRepository.java
@@ -0,0 +1,16 @@
+package com.expense.repository;
+
+//import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.expense.model.Category;
+
+@Repository
+@Transactional
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+	// List<Category> findAllCategories();
+}
