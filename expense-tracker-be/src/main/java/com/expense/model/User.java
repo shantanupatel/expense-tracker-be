786135b784a0c17a09d224cb5c97ae829790512a
Shantanu Patel <shantu.patel@gmail.com>
2024-09-03 10:43:45 +0530

    User entity created for storing user related information.

---
+++ expense-tracker-be/src/main/java/com/expense/model/User.java
@@ -0,0 +1,118 @@
+package com.expense.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SourceType;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private boolean isActivated;
+	private boolean isActive;
+
+	@CreationTimestamp(source = SourceType.DB)
+	private LocalDateTime createdDate;
+
+	@UpdateTimestamp(source = SourceType.DB)
+	private LocalDateTime updatedDate;
+
+	public User() {
+	}
+
+	public User(String firstName, String lastName, String email, boolean isActivated, boolean isActive,
+			LocalDateTime createdDate, LocalDateTime updatedDate) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.isActivated = isActivated;
+		this.isActive = isActive;
+		this.createdDate = createdDate;
+		this.updatedDate = updatedDate;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isActivated() {
+		return isActivated;
+	}
+
+	public void setActivated(boolean isActivated) {
+		this.isActivated = isActivated;
+	}
+
+	public boolean isActive() {
+		return isActive;
+	}
+
+	public void setActive(boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public LocalDateTime getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(LocalDateTime createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public LocalDateTime getUpdatedDate() {
+		return updatedDate;
+	}
+
+	public void setUpdatedDate(LocalDateTime updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", isActivated=" + isActivated + ", isActive=" + isActive + ", createdDate=" + createdDate + ", updatedDate="
+				+ updatedDate + "]";
+	}
+
+}
