2c5ae6f42f15aac917c1fdc2c41138a984daccce
Shantanu Patel <shantu.patel@gmail.com>
2024-09-05 17:54:12 +0530

    CategoryController class updated with handler function for getting category on the basis of provided id.

--- expense-tracker-be/src/main/java/com/expense/controller/CategoryController.java
+++ expense-tracker-be/src/main/java/com/expense/controller/CategoryController.java
@@ -1,15 +1,18 @@
 package com.expense.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.expense.handler.ResponseHandler;
 import com.expense.model.Category;
 import com.expense.service.CategoryService;
 
@@ -37,8 +40,6 @@ public class CategoryController {
 	@PostMapping(consumes = { "application/json" })
 	public ResponseEntity<Category> createCategory(@RequestBody Category category) throws Exception {
 
-		System.out.println(category);
-
 		Category newCategory = new Category();
 
 		newCategory.setCategoryName(category.getCategoryName());
@@ -47,6 +48,20 @@ public class CategoryController {
 		Category createdCategory = categoryService.createCategory(newCategory);
 
 		return new ResponseEntity<>(createdCategory, HttpStatus.CREATED);
+
+	}
+
+	@Operation(summary = "Get Category by specified ID", description = "Get Category by specified ID. The response is an Category object with id, category name, created by, created date and last updated date.")
+	@GetMapping("/{categoryId}")
+	public ResponseEntity<Object> getCategory(@PathVariable Integer categoryId) {
+
+		Optional<Category> categoriesData = categoryService.findCategory(categoryId);
+
+		if (categoriesData.isPresent()) {
+			return ResponseHandler.generateResponse(null, HttpStatus.OK, categoriesData);
+		} else {
+			return ResponseHandler.generateResponse("No matching record found", HttpStatus.NOT_FOUND, categoriesData);
+		}
 
 	}
 
