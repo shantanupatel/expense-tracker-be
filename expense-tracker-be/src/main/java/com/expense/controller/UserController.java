1d5d13d14d5e9a56c1fb891721764a9865dfd597
Shantanu Patel <shantu.patel@gmail.com>
2024-09-05 12:03:02 +0530

    UserController class updated with annotation for operation for providing a summary and description

--- expense-tracker-be/src/main/java/com/expense/controller/UserController.java
+++ expense-tracker-be/src/main/java/com/expense/controller/UserController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.expense.model.User;
 import com.expense.service.UserService;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -23,6 +25,8 @@ public class UserController {
 		this.userService = userService;
 	}
 
+	// @Tag(name = "get", description = "GET methods of User APIs")
+	@Operation(summary = "Get list of all Users", description = "Get list of all Users. The response is a list of Users object with id, first name last name, email, activated, active, creation date, last updated date and last login date.")
 	@GetMapping()
 	public List<User> getUsers() throws Exception {
 
@@ -30,6 +34,8 @@ public class UserController {
 
 	}
 
+	// @Tag(name = "post", description = "POST methods of User APIs")
+	@Operation(summary = "Create a new User", description = "Create a new User. The response is an User object with id, first name last name, email, activated, active, creation date, last updated date and last login date.")
 	@PostMapping()
 	public ResponseEntity<User> createUser(@RequestBody User user) {
 
